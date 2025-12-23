4c5e6a98defc5967865ae4c444f7f6987d4bf144
Yamini-519 <24331a05i9@mvgrce.edu.in>
2025-12-23 19:09:01 +0530

    Add files via upload

---
+++ nparray.py
@@ -0,0 +1,8 @@
+marks=[2,3,4,5,6,6,1,7,8,9]    
+print("length of array:",len(marks))
+print("marks:",marks)
+print("type of marks:",type(marks)) 
+import numpy as np  
+npmarks=np.array(marks)    
+print("length of marks using nparray:",npmarks)
+print("type of marks using nparray:",type(npmarks))  
